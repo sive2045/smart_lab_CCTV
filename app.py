bf77dcdc9aced5385d43b944a73d8ba1706f6b42
sive2045 <lc9902130509@gmail.com>
2021-08-02 21:25:23 +0900

    init sign-up and databse

--- app.py
+++ app.py
@@ -1,7 +1,58 @@
-from flask import Flask
+from flask import Flask, jsonify, request, current_app
+from sqlalchemy import create_engine, text
 
-app = Flask(__name__)
 
-@app.route("/ping", methods=['GET'])
-def ping():
-    return "pong"
+def create_app(test_config = None):
+    """
+    To do:
+    database connect!
+    """
+    app = Flask(__name__)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+    
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow=0)
+    app.database = database
+
+    @app.route("/sign-up", methods=['POST'])
+    def sign_up():
+        new_user = request.json
+        new_user_id = app.database.execute(text("""
+        INSERT INTO users (
+            name,
+            email,
+            profile,
+            hashed_password
+        ) VALUES (
+            :name,
+            :email,
+            :profile,
+            :password
+        )
+        """), new_user).lastrowid
+
+        row = current_app.database.execute(text("""
+        SELECT
+            id,
+            name,
+            email,
+            profile
+        FROM users
+        WHERE id = :user_id
+        """), {
+            'user_id' : new_user_id
+        }).fetchone()
+
+        created_user ={
+            'id'    : row['id'],
+            'name'  : row['name'],
+            'email' : row['email'],
+            'profile':row['profile']
+        } if row else None
+
+        return jsonify(created_user)
+
+    return app
